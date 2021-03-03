f205ace5b2eb93b34f6066e488da9594027c6030
Nihala98 <79505981+Nihala98@users.noreply.github.com>
2021-03-03 18:33:35 +0530

    Update main.php

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -113,6 +113,7 @@ public function log()
 	redirect('main/log','refresh');
 	}
 }
+//*******User profile update********//	
 public function user_update()
         {
         if($_SESSION['logged_in']==true&& $_SESSION['utype']=='0')
@@ -147,6 +148,7 @@ public function update()
 			
 		}
 	}
+	//**********user view**************//
 	public function view_table()
 	{
 		if($_SESSION['logged_in']==true&& $_SESSION['utype']=='0')
@@ -166,6 +168,7 @@ public function admin_home()
 {
 	$this->load->view("admin_home");
 }
+//*********user view and manage*********//	
 public function user_table()
 {
 if($_SESSION['logged_in']==true&& $_SESSION['utype']=='1')
@@ -267,6 +270,62 @@ public function delete()
                      echo '<label class="text-success"><span class="glyphicon glyphicon-ok"></span> </label>';  
                 }  
            }
-           
+//******send mail*********//
+  public function forget()
+{
+$this->load->view('forgetpswd');
+}
+
+public function send()
+{
+    $to =  $this->input->post('from');  // User email pass here
+    $subject = 'Welcome To Elevenstech';
+
+    $from = 'brainybunch2021@gmail.com';              // Pass here your mail id
+
+    $emailContent = '<!DOCTYPE><html><head></head><body><table width="600px" style="border:1px solid #cccccc;margin: auto;border-spacing:0;"><tr><td style="background:#000000;padding-left:3%"><img src="http://elevenstechwebtutorials.com/assets/logo/logo.png" width="300px" vspace=10 /></td></tr>';
+    $emailContent .='<tr><td style="height:20px"></td></tr>';
+
+
+    $emailContent .= $this->input->post('message');  //   Post message available here
+
+
+    $emailContent .='<tr><td style="height:20px"></td></tr>';
+    $emailContent .= "<tr><td style='background:#000000;color: #999999;padding: 2%;text-align: center;font-size: 13px;'><p style='margin-top:1px;'><a href='http://elevenstechwebtutorials.com/' target='_blank' style='text-decoration:none;color: #60d2ff;'>www.elevenstechwebtutorials.com</a></p></td></tr></table></body></html>";
+               
+
+
+    $config['protocol']    = 'smtp';
+    $config['smtp_host']    = 'ssl://smtp.gmail.com';
+    $config['smtp_port']    = '465';
+    $config['smtp_timeout'] = '60';
+
+    $config['smtp_user']    = 'brainybunch2021@orisys@gmail.com';    //Important
+    $config['smtp_pass']    = 'brainybunch04';  //Important
+
+    $config['charset']    = 'utf-8';
+    $config['newline']    = "\r\n";
+    $config['mailtype'] = 'html'; // or html
+    $config['validation'] = TRUE; // bool whether to validate email or not
+
+     
+
+    $this->email->initialize($config);
+    $this->email->set_mailtype("html");
+    $this->email->from($from);
+    $this->email->to($to);
+    $this->email->subject($subject);
+    $this->email->message($emailContent);
+    $this->email->send();
+
+    $this->session->set_flashdata('msg',"Mail has been sent successfully");
+    $this->session->set_flashdata('msg_class','alert-success');
+    return redirect('main/forget');
+}
+
+
 
     }
+  
+
+    
